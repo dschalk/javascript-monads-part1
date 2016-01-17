ae17fbac083544aff76ec55073a88d4098c59eeb
dschalk <schalkbox@gmail.com>
2016-01-16 20:57:13 -0500

    Added a feature to main.js

--- js/main.js
+++ js/main.js
@@ -50,9 +50,9 @@ var styleRl = style2;
 function view(m1, m2, m3, m4, m5, m6, m7, m8, mI1, mI2, hello) { 
     return h('div',{style: style3}, 
     [  h('div',{style: { width: '65%', textAlign: 'left', marginLeft: 40, marginRight: '17%', fontSize: '24px'}}, 
-    [ h('h2', {style: {textAlign: 'center', color:  '#BBFFFF'}}, 'Fun With Monads - UNDER RECONSTRUCTION'),
+    [ h('h2', {style: {textAlign: 'center', color:  '#BBFFFF'}}, 'Javascript Monads Part 1'),
       h('span', {style: {marginLeft: '18px'}},  'The code for this single-page site is at ' ),
-      h('a', {props: {href: 'https://github.com/dschalk/javascript-monads-part1'}, style: {color: '#EECCFF'}}, 'fun_with_monads' ),
+      h('a', {props: {href: 'https://github.com/dschalk/javascript-monads-part1'}, style: {color: '#EECCFF'}}, 'javascript-monads-part1' ),
       h('span', ' If pressing F12 switches your browser to a console, I think you will find that you have access to all of the monads and functions being used in this presentation, Try entering "mM1.ret("Hello world")" on the command like. Press F12 again and roll over (don\'t click it) the RE-SET button at the bottom of the right column. When the column gets updated, the new value of mM1.x that you created should appear. '  ),
       h('h3', 'This Series Is For Web Developers' ),
      h('p', 'This is not about category theory or the lambda calculus. I call my little inventions "monads", and I am sorry if that drives away that dwindling breed of developers who strive for little more than to master JQuery and whatever frameword they are using. I developed the manads to make my work easier and more satisfying, and now I am taking time out to share them with whomever is interested. ' ),
@@ -77,6 +77,10 @@ function view(m1, m2, m3, m4, m5, m6, m7, m8, mI1, mI2, hello) {
       h('pre',
 '   cube(add(add(mM1,-mM1.x),3))  '           ),    
       h('p', ' "mM1.bnd(cube)" does exactly what "cube(mM1)" does. They both return mM1 after cubing its value, or return mM1 with a value of NAN if its value was not a number. In the above expression, the innermost call to "add" sets mM1.x to 0; the next call adds 3, and cube yields mM1.x = 27, no matter what the value of mM1 was before. The above expression can make a person cross-eyed, so I will stick with linking objects when nesting operations.'   ),
+      h('h3',  'Anonymous Monads' ),
+      h('p', 'Chains of computations can be performed anonymously, releasing the final value into a named monad or releasing it as a side-effect in the very last step. Here\'s an example: '  ),
+   h('button', {on: { mouseenter: update6e, mouseleave: update6l, click: updateAnon }, style: style6},
+               'new Monad(0).bnd(add,3).bnd(cube).bnd(x => mM1.ret(x.x))'  ),
       h('p', 'Before wrapping up this first installment, I think I should provide a "Hello world." program. Here it is: '  ),
    h('button', {on: { mouseenter: update4e, mouseleave: update4l, click: updateHello }, style: style5}, [
    h('pre',
@@ -144,11 +148,19 @@ function update0(event) {
 }
 
 function updateR(event) {
-  mM1.ret(0).bnd(mM2.ret).bnd(mM3.ret).bnd(mM4.ret).bnd(mM5.ret)
-  .bnd(mM6.ret).bnd(mM7.ret).bnd(mM8.ret).bnd(mMI1.ret).bnd(mMI2.ret);  
+  mM1.ret(0);
+  mM2.ret(0);
+  mM3.ret(0);
+  mM4.ret(0);
+  mM5.ret(0);
+  mM6.ret(0);
+  mM7.ret(0);
+  mM8.ret(0);
+  hello.ret('Goodbye');
   update0();
-}
-
+}       
+       
+       
 function update(event) {
   mM2.bnd(v => add(v,mM2,5).bnd(cube));
   update0();
@@ -179,6 +191,13 @@ function updateHello() {
   hello.ret('Hello world.'); 
   update0();
 }
+
+
+function updateAnon() {
+  new Monad(0).bnd(add,3).bnd(cube).bnd(x => mM1.ret(x.x))
+  update0();
+}
+
 
 function updateDemo2() {
   add(3, mM1)
@@ -314,6 +333,16 @@ function update5e(event) {
 
 function update5l(event) {
   style5 = style2;
+  update0();
+}
+
+function update6e(event) {
+  style6 = style1;
+  update0();
+}
+
+function update6l(event) {
+  style6 = style2;
   update0();
 }
 
